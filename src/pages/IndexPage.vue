61400319490d5ce8448956bedafd4d65b7109166
Bruck-3 <brucktafesse@yahoo.com>
2024-04-18 12:34:07 +0300

    added prettier

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,16 +2,13 @@
   <q-page class="">
     <h1 class="headline text-limegreen-900 text-center">CLoIT</h1>
   </q-page>
-
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
 })
 </script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
