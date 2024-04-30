6a10c271aeaf42c44a7b435f611d7d186a637490
Bruck-3 <brucktafesse@yahoo.com>
2024-04-30 15:42:39 +0300

    image src

--- src/components/organisms/portal-product/my-page/mypage-notice/notice-list/NoticeList.vue
+++ src/components/organisms/portal-product/my-page/mypage-notice/notice-list/NoticeList.vue
@@ -7,7 +7,8 @@
         <p class="notice" v-html="formattedNotice"></p>
       </div>
       <div class="col-3">
-        <ImagePlaceholder />
+        <img v-if="imageSrc" :src="imageSrc" alt="">
+        <ImagePlaceholder v-else />
       </div>
     </div>
   </div>
@@ -28,6 +29,10 @@ const props = defineProps({
     type: String,
     default: `안녕하세요, 클로잇입니다. 24년 4월 21일에 전체 서비스 점검이 있을 예정입니다. 서비스 점검 시간은 10:00 am ~ 18:00 pm이며 이 시간동안 대시보드 접근 및 대시보드 내에서 행해지는 모든 액션 사용이 불가합니다.\n\n
 이용에 불편을 드려 죄송합니다. 더 나은 서비스로 찾아뵙기 위함이니 양해 부탁드립니다. 더 자세한 사항은....`
+  },
+  imageSrc: {
+    type: String,
+    default: ''
   }
 });
 
