1371600523d24b342f64208673d1fa019afb6264
Bruck-3 <brucktafesse@yahoo.com>
2024-05-03 16:21:29 +0300

    dropdown select for language change

--- src/components/molecules/form-components/drop-down/DropDown.vue
+++ src/components/molecules/form-components/drop-down/DropDown.vue
@@ -1,3 +1,80 @@
-<template></template>
-<script></script>
-<style></style>
+<template>
+  <q-select
+    ref="select"
+    v-model="model"
+    borderless
+    dense
+    :options="filteredOptions"
+    :input="onOptionSelect"
+    class="select-container"
+    :dropdown-icon="'keyboard_arrow_down'"
+    :style="{ maxWidth: maxWidth, height: height }"
+  >
+    <template v-slot:prepend>
+      <q-icon :name="iconLeft" class="prepend-icon" />
+    </template>
+  </q-select>
+  <div v-if="description" class="select-description">{{ description }}</div>
+</template>
+<script setup>
+import { ref, computed, defineProps, defineEmits } from 'vue'
+
+// Define props and emits
+const props = defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+  placeholder: {
+    type: String,
+    default: 'Select item',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  maxWidth: {
+    type: String,
+    value: '',
+  },
+  height: {
+    type: String,
+    value: '',
+  },
+  iconLeft: {
+    type: String,
+    value: '',
+  },
+  options: {
+    type: Array,
+    default: () => [
+      { value: 'line', label: 'Preview' },
+      { value: 'rectangle', label: '레포트' },
+    ],
+  },
+})
+
+const emit = defineEmits(['file'])
+
+const model = ref(null)
+const searchQuery = ref('')
+
+// Define a computed property for filtering options based on the search query
+const filteredOptions = computed(() => {
+  const needle = searchQuery.value.toLowerCase()
+  return props.options.filter((option) =>
+    option.label.toLowerCase().includes(needle),
+  )
+})
+
+// Define a function to handle option selection
+function onOptionSelect(option) {
+  console.log(option.value)
+  model.value = option.value // Update the model when an option is selected
+  emit('file', model.value) // Emit the 'file' event with the model value
+}
+</script>
+
+<style scoped lang="scss">
+@import 'DropDown.style.scss';
+</style>
--- src/components/organisms/portal-product/my-page/header/HeaderBar.vue
+++ src/components/organisms/portal-product/my-page/header/HeaderBar.vue
@@ -6,18 +6,27 @@
       class="notification-icon nav-icons"
       name="notifications_none"
     ></q-icon>
+    <DropDown
+      class="dropdown-icon"
+      iconLeft="language"
+      maxWidth="133px"
+      height="52px"
+      label="DropDown"
+      :options="options"
+    />
     <StandardButton
       :type="ctaButton.type"
       :label="ctaButton.label"
       :outlined="ctaButton.outlined"
       buttonWidth="200px"
+      class="standard-button"
     />
     <q-icon class="logout-icon nav-icons" name="logout"></q-icon>
   </q-header>
 </template>
 <script setup>
 import StandardButton from '../../../../molecules/buttons/standard-buttons/StandardButtons.vue'
-
+import DropDown from '../../../../molecules/form-components/drop-down/DropDown.vue'
 const props = defineProps({
   ctaButton: {
     type: Object,
@@ -28,6 +37,16 @@ const props = defineProps({
     }),
   },
 })
+const options = [
+  {
+    label: 'En',
+    value: 'en',
+  },
+  {
+    label: 'Kr',
+    value: 'korean',
+  },
+]
 </script>
 <style lang="scss" scoped>
 @import 'Header.style.scss';
