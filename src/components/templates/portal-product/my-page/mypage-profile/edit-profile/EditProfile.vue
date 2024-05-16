99da371417566a9ebedfc53f4a5468b18ad040ff
workuMekuriya <mekuriya185@gmail.com>
2024-05-16 20:21:46 +0300

    Issue #5504

--- src/components/templates/portal-product/my-page/mypage-profile/edit-profile/EditProfile.vue
+++ src/components/templates/portal-product/my-page/mypage-profile/edit-profile/EditProfile.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
     <div class="breadcrumb-container">
-      <div class="breadcrumb-item">My Profile</div>
+      <div class="breadcrumb-item">My Prof<span style="font-family: 'Plus Jakarta Sans'">i</span>le</div>
       <div class="breadcrumb-separator">〉</div>
-      <span class="breadcrumb-item breadcrumb-link">Edit Profile</span>
+      <span class="breadcrumb-item breadcrumb-link">Edit Prof<span style="font-family: 'Plus Jakarta Sans'">i</span>le</span>
     </div>
-    <div class="title">Edit Profile</div>
+    <div class="title">Edit Prof<span style="font-family: 'Plus Jakarta Sans'">i</span>le</div>
     <div class="grid-container">
       <div class="grid-item">
         <TextField
@@ -95,20 +95,20 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import RoundedButton from 'src/components/molecules/buttons/conditionally-styled-buttons/ConditionalStyleRoundedButton.vue'
-import TextField from 'src/components/molecules/form-components/fields/textfield/TextField.vue'
-import SelectDropdown from 'components/molecules/form-components/select-dropdown/SelectDropdown.vue'
+import { ref } from "vue";
+import RoundedButton from "src/components/molecules/buttons/conditionally-styled-buttons/ConditionalStyleRoundedButton.vue";
+import TextField from "src/components/molecules/form-components/fields/textfield/TextField.vue";
+import SelectDropdown from "components/molecules/form-components/select-dropdown/SelectDropdown.vue";
 
 // Define the 'nextStep' event
-const emits = defineEmits(['nextStep'])
+const emits = defineEmits(["nextStep"]);
 
 // Method to handle the save button click
 const handleSaveClick = () => {
-  emits('nextStep', 0)
-}
+  emits("nextStep", 0);
+};
 </script>
 
 <style lang="scss" scoped>
-@import 'EditProfile.styles.scss';
+@import "EditProfile.styles.scss";
 </style>
--- src/components/templates/portal-product/my-page/mypage-profile/inquiry-history/inquiry-detail/InquiryDetail.vue
+++ src/components/templates/portal-product/my-page/mypage-profile/inquiry-history/inquiry-detail/InquiryDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="breadcrumb">
-    <div class="help-center">My Profile</div>
+    <div class="help-center">My Prof<span style="font-family: 'Plus Jakarta Sans'">i</span>le</div>
     <div class="breadcrumb-separator">〉</div>
     <span class="help-center">Inquiry</span>
     <div class="breadcrumb-separator">〉</div>
